caf5c75cefe85ef07e08a0d6dd6011b2a1996c5b
Denys M <dector@dector.space>
2021-01-30 16:59:17 +0400

    Add sample as a separate project

---
+++ sample/build.gradle.kts
@@ -0,0 +1,25 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.21"
+}
+
+dependencies {
+    implementation("space.dector.tuyalib:library:0.1.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+    implementation("com.eclipsesource.minimal-json:minimal-json:0.9.5")
+}
+
+repositories {
+    jcenter()
+    maven("https://dl.bintray.com/dector/tuya-lib")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.freeCompilerArgs += listOf(
+        "-Xopt-in=kotlin.time.ExperimentalTime",
+        "-Xopt-in=kotlin.RequiresOptIn"
+    )
+}
