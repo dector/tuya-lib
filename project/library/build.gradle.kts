ae8e148f19e556e0e1f2d36a0732b4758c75ac5f
Denys M <dector@dector.space>
2021-02-07 16:37:22 +0400

    Use MavenCentral for publishing
    RIP JCenter

--- project/library/build.gradle.kts
+++ project/library/build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm")
 
-    id("com.jfrog.bintray") version "1.8.5"
     `maven-publish`
+    signing
 }
 
 dependencies {
@@ -37,32 +37,6 @@ val test by tasks.getting(Test::class) {
     useJUnitPlatform()
 }
 
-bintray {
-    val secrets = runCatching {
-        rootDir.resolve(".secrets/bintray").readLines()
-    }.getOrNull()
-
-    if (secrets != null) {
-        user = secrets[0]
-        key = secrets[1]
-
-        pkg.apply {
-            repo = "tuya-lib"
-            name = "library"
-
-            setLicenses("Apache-2.0")
-            vcsUrl = "https://github.com/dector/tuya-lib"
-
-            version.name = Publication.versionName
-            version.vcsTag = Publication.vcsTag
-        }
-
-        setPublications("library")
-    } else {
-        logger.warn("Bintray secrets not found")
-    }
-}
-
 publishing {
     publications {
         create<MavenPublication>("library") {
@@ -71,13 +45,76 @@ publishing {
             groupId = project.group.toString()
             artifactId = "library"
             version = Publication.versionName
+
+            pom {
+                name.set("library")
+                description.set("Library to control Tuya devices")
+                url.set("https://github.com/dector/tuya-lib")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("dector")
+                        name.set("dector")
+                        email.set("github@dector.space")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:github.com/dector/tuya-lib")
+                    developerConnection.set("scm:git:ssh://github.com/dector/tuya-lib.git")
+                    url.set("https://github.com/dector/tuya-lib")
+
+                    tag.set(Publication.vcsTag)
+                }
+
+                // Include transitive dependencies
+                withXml {
+                    val dependenciesNode = asNode().appendNode("dependencies")
+
+                    project.configurations.implementation.get().allDependencies.forEach {
+                        val node = dependenciesNode.appendNode("dependency")
+                        node.appendNode("groupId", it.group)
+                        node.appendNode("artifactId", it.name)
+                        node.appendNode("version", it.version)
+                    }
+                }
+            }
         }
     }
 
     repositories {
         maven {
+            name = "sonatype"
+            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+
+            val secrets = runCatching {
+                rootDir
+                    .resolve(".secrets/sonatype")
+                    .readLines()
+                    .filter(String::isNotBlank)
+            }.getOrNull()
+
+            if (secrets != null) {
+                credentials {
+                    username = secrets[0].trim()
+                    password = secrets[1].trim()
+                }
+            } else {
+                logger.warn("Sonatype secrets not found")
+            }
+        }
+        maven {
             name = "localRepo"
             url = uri("file://${rootDir}/repo")
         }
     }
+}
+
+signing {
+    sign(publishing.publications)
 }
